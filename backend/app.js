4910d6dceb0c603a1cdd8a8e0ba644d9e7562df7
YanB25 <yanb25@mail2.sysu.edu.cn>
2019-04-14 19:25:18 +0800

    update backend

--- backend/app.js
+++ backend/app.js
@@ -15,13 +15,18 @@ const connectionString = `postgresql://dwh:dwh@127.0.0.1:5432/dwh?sslmode=disabl
 let pgClient = new Client(connectionString);
 
 app.get('/helloworld', async (req, res) => {
-    await pgClient.connect();
     console.log('get request');
     let query = await pgClient.query(
         "select id, masterid, createdts, dealid, type, status, authorid, counterpartyid, duration, price, netflags, identitylevel, blacklist, tag, frozensum creatoridentitylevel, creatorname, creatorcountry from orders;");
     console.log(query);
     res.status(200).jsonp({data: query.rows, errno: 0, msg: 'ok'});
-    await pgClient.end();
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, async () => {
+    await pgClient.connect();
+    console.log(`Example app listening on port ${port}!`)
+});
+
+process.on('exit', async () => {
+    await pgClient.end();
+})
